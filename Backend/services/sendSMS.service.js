425d33c1e91be541354019dc97bf8443bf22acdd
Priyadharsan T <priyadharsant4@gmail.com>
2026-09-15 09:00:54 +0530

    changes

--- Backend/services/sendSMS.service.js
+++ Backend/services/sendSMS.service.js
@@ -60,16 +60,16 @@ const sendSMS = async (phone_number_parent, message) => {
 
     // phoneNumber is accepted but ignored during testing
     console.log("Original destination:", phoneNumber);
-    console.log("Testing destination:", phoneNumber);
+    console.log("Message :", message);
 
-    const command = new SendTextMessageCommand({
-      // Always send to .env phone during testing
-      DestinationPhoneNumber: phoneNumber,
+    // const command = new SendTextMessageCommand({
+    //   // Always send to .env phone during testing
+    //   DestinationPhoneNumber: phoneNumber,
 
-      MessageBody: message,
+    //   MessageBody: message,
 
-      MessageType: "TRANSACTIONAL",
-    });
+    //   MessageType: "TRANSACTIONAL",
+    // });
 
     const response = await awsSmsClient.send(command);
 
